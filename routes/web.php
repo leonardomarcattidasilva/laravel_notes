bb1a5f8dcda7a64e5267b39e49e027c8be3c321e
Leonardo Marcatti da Silva <leonardomarcatti@hotmail.com>
2024-12-21 11:12:58 -0300

    MainController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
+
+
 Route::get('/', function () {
-    return view('welcome');
+    return 'ok';
 });
+
+Route::get('main', [MainController::class, 'index']);
